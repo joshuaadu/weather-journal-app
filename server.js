02d693c9cdf4c523ab198c050668b23a28c45f61
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2021-08-18 18:44:31 +0000

    feat: GET project data, POST data to project data

--- server.js
+++ server.js
@@ -2,18 +2,43 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server
+/** Setup Server */ 
+// Assign server port
+const port = 8082;
+
+// Start server
+const server = app.listen(port, (req, res) => {
+    console.log('Server running on localhost:', port);
+})
+
+// GET route for project data
+app.get('/all', (req, res) => {
+    projectData[1] = 'first data in project data file';
+    res.send(projectData);
+})
+
+// POST route to add incoming data to projectData
+app.post('/postData', (req, res) => {
+    Object.assign(projectData, req.body);
+    res.send(projectData);
+})
--- website/app.js
+++ website/app.js
@@ -3,3 +3,37 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// GET Project Data
+const getProjectData = async (url='/all') => {
+    const request = await fetch(url);
+    try {
+        const data = await request.json();
+        console.log(data);
+    }catch(error){
+        console.log('error', error)
+    }
+};
+
+// Post data to project data
+const postData = async (url, data={}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const postMessage = await response.json();
+        console.log(postMessage);
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+postData('/postData', {3: 'Another data posted to project data'});
+getProjectData();
+
